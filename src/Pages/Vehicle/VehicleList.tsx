53ebbae7d2275210fb61019a4654d74650a80fd6
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2025-04-30 23:21:07 +0300

    backend duzenlemeleri sonrasi resimler getirildi.

--- src/Pages/Vehicle/VehicleList.tsx
+++ src/Pages/Vehicle/VehicleList.tsx
@@ -3,45 +3,40 @@ import { useGetVehiclesQuery } from '../../Api/vehicleApi'
 import { vehicleModel } from '../../Interfaces/vehicleModel'
 
 function VehicleList() {
-
-    const {data, isLoading} = useGetVehiclesQuery(null)
+    const { data, isLoading } = useGetVehiclesQuery(null)
     const [vehicles, setVehicleState] = useState<vehicleModel[]>([])
 
-    useEffect(()=>{
-      if (data ) {
-        setVehicleState(data.result);
-       
-  
-      }
-     
-    },[data])
+    useEffect(() => {
+        if (data && data.$values) {
+            setVehicleState(data.$values)
+        }
+    }, [data])
 
 
-  return (
-    <div className='container'>
-
-      {
-        vehicles.map((vehicle,index) => {
-          return (
-            <div className='auction-card text-center'> 
-            <div className='card-image text-center'>
-              <img src={vehicle.image} > 
-              </img>
-            </div>
-            <div className='card-details text-center'>
-              <h2></h2>
-              <p><strong></strong></p>
-              <p><strong></strong></p>
-              <p><strong></strong></p>
-              <p><strong></strong></p>
-            </div>
-          </div>
-
-          )
-      })
-}
-</div>
-  )
+    return (
+        <div className='container'>
+            {vehicles.length > 0 ? (
+                vehicles.map((vehicle, index) => (
+                    <div key={index} className='auction-card text-center'>
+                        <div className='card-image text-center'>
+                            <img src={vehicle.image} alt="" />
+                        </div>
+                        <div className='card-details text-center'>
+                            <h2>{vehicle.brandAndModel}</h2>
+                            <p><strong>Year:</strong> {vehicle.manufacturingYear}</p>
+                            <p><strong>Price:</strong> {vehicle.price}</p>
+                            <p><strong>Mileage:</strong> {vehicle.mileage}</p>
+                            <p><strong>Color:</strong> {vehicle.color}</p>
+                            <p><strong>Engine Capacity:</strong> {vehicle.engineCapacity}</p>
+                            <p><strong>Plate Number:</strong> {vehicle.plateNumber}</p>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div>No vehicles found</div>
+            )}
+        </div>
+    )
 }
 
 export default VehicleList
