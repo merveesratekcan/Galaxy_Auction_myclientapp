1ed29182c5e25411d0dcd823e344449a2bdf90c0
Merve Esra Tekcan <esramerve.tekcan@gmail.com>
2025-07-31 22:46:23 +0300

    Currend Bid degerinin gosterilmesi-tekliflerin siralanmasi- filtrelemeye giris

--- src/Pages/Vehicle/VehicleList.tsx
+++ src/Pages/Vehicle/VehicleList.tsx
@@ -16,26 +16,27 @@ const VehicleList: React.FC<VehicleListProps> = ({ vehicles }) => {
             <div className="page-center">
                 {vehicles.map((vehicle, index) => (
                     <div key={index} className="auction-card text-center">
-                        <div className="card-image text-center">
-                            <img
-                                src={vehicle.image || 'https://via.placeholder.com/150'}
-                                alt={vehicle.brandAndModel}
-                            />
-                        </div>
-                        <div className="card-details text-center">
-                            <h2>{vehicle.brandAndModel}</h2>
-                            <p><strong>Year:</strong> {vehicle.manufacturingYear}</p>
-                            <p><strong>Color:</strong> {vehicle.color}</p>
-                            <p><strong>Current Bid:</strong> ${vehicle.price}</p>
-                            <p><strong>End Time:</strong> {vehicle.endTime}</p>
-                        </div>
-                        <div className="card-action text-center">
-                            <Link to={`Vehicle/VehicleId/${vehicle.vehicleId}`}>
-                                <button className="btn btn-danger">Detail</button>
-                            </Link>
-                        </div>
-                        <Circle vehicle={vehicle} />
+                    <div className="card-image text-center">
+                        <img
+                            src={vehicle.image || 'https://via.placeholder.com/150'}
+                            alt={vehicle.brandAndModel}
+                        />
                     </div>
+                    <div className="card-details text-center">
+                        <span className="badge">{vehicle.brandAndModel.split(' ')[0]}</span>
+                        <h2>{vehicle.brandAndModel}</h2>
+                        <p><strong>Year:</strong> {vehicle.manufacturingYear}</p>
+                        <p><strong>Color:</strong> {vehicle.color}</p>
+                        <p><strong>Current Bid:</strong> ${vehicle.price}</p>
+                        <p><strong>End Time:</strong> {vehicle.endTime}</p>
+                    </div>
+                    <div className="card-action text-center">
+                        <Link to={`Vehicle/VehicleId/${vehicle.vehicleId}`}>
+                            <button className="btn btn-danger">Detail</button>
+                        </Link>
+                    </div>
+                    <Circle vehicle={vehicle} />
+                </div>
                 ))}
             </div>
         </>
